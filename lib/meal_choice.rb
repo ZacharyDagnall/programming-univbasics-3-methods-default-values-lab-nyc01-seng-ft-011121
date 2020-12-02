8ee9e5fba09c9f0678123734459f5149b9c93d3d
Zachary R Dagnall <zachary.dagnall@gmail.com>
2020-12-02 21:21:28 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,3 +6,5 @@ def meal_choice(veg1, veg2, protein="tofu")
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
   return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
+
+#Why won't the test allow me to use the p method instead of the puts method and the last line?? The p method can accomplish both in one line, right?
