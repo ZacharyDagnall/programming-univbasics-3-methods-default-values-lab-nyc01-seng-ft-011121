c7f93345adae8db4b49aa8ffccbdaf43cb1859ee
Zachary R Dagnall <zachary.dagnall@gmail.com>
2020-12-02 21:21:34 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,10 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+
+def meal_choice(veg1, veg2, protein="tofu")
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  return "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
+#Why won't the test allow me to use the p method instead of the puts method and the last line?? The p method can accomplish both in one line, right?
